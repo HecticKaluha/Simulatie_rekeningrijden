cf6889e8ca0e6dee90c62a6a71cacb98d7cd9d01
HecticKaluha <stefanoverhoeve44@gmail.com>
2018-03-14 14:20:48 +0100

    Added simulation across the line by lon and lat
    Also a random car id had been added to the line object

--- js/simulation/simulate_v3.js
+++ js/simulation/simulate_v3.js
@@ -2,6 +2,7 @@ var directionsDisplay = new google.maps.DirectionsRenderer({ draggable: true });
 var directionsService = new google.maps.DirectionsService();
 var map;
 var cars = 0;
+var secondsToMilliSeconds = 1000;
 
 
 window.onload = init;
@@ -31,7 +32,9 @@ function calcRoute(startpoint, endpoint) {
             directionsDisplay.setDirections(response);
             //document.getElementById('Gresponse').innerHTML = JSON.stringify(response);
 			var line = createPolyline(response);
-			animate(line);
+			var route = response.routes[0];
+			newAnimate(response, line);
+			//animate(line);
 			cars++;
 			updateGui(response, line);
         }
@@ -40,18 +43,20 @@ function calcRoute(startpoint, endpoint) {
 
 function createPolyline(directionResult) {
   var line = new google.maps.Polyline({
-      path: directionResult.routes[0].overview_path,
-      strokeColor: '#FF0000',
-      strokeOpacity: 0.5,
-      strokeWeight: 4,
-      icons: [{
-          icon: {
-            path: google.maps.SymbolPath.CIRCLE,
-            scale: 5,
-            strokeColor: '#393'
-          },
-          offset: '100%'
-        }]
+		path: directionResult.routes[0].overview_path,
+		strokeColor: '#FF0000',
+		strokeOpacity: 0.5,
+		strokeWeight: 4,
+		icons: [{
+		  icon: {
+			path: google.maps.SymbolPath.CIRCLE,
+			scale: 5,
+			strokeColor: '#393'
+		  },
+		  //100 / (totaal aantal punten / punt waar je bent )
+		  offset: '0%'
+			}],
+		carId: Math.floor((Math.random() * 100000)+1)
   });
   line.setMap(map);
   return line;
@@ -70,30 +75,63 @@ function animate(line) {
 
 
 
-function newAnimate()
+function newAnimate(route, line)
 {
-	var stepCount = 0;
-	/*window.setInterval(){
+	//console.log(line.carId);
+	//console.log("duration = " + route.routes[0].legs[0].duration.value);
+	//console.log(route.routes[0].overview_path.length);
+	var avgTimePerStep = (route.routes[0].legs[0].duration.value) / (route.routes[0].overview_path.length);
+	//console.log("avgTimePerStep is " + avgTimePerStep);
+	var stepCoordinates =
+	{
+	};
+	var i = 0;
+	var intervalForRoute = window.setInterval(function() {
+		i++;
 		
-	}*/
+		if(i < route.routes[0].overview_path.length)
+		{
+			value = route.routes[0].overview_path[i];
+			stepCoordinates.stepLat = value.lat().toFixed(6);
+			stepCoordinates.stepLon = value.lng().toFixed(6);
+			moveToStep(stepCoordinates, i, line, route)
+			//console.log(stepCoordinates);
+		}
+		else{
+			console.log("done");
+			clearInterval(intervalForRoute);
+		}
+			
+		}, avgTimePerStep* secondsToMilliSeconds)	
 	
 }
+
+
+function moveToStep(stepCoordinates, index, line, route)
+{
+	console.log("step " + index);
+	var icons = line.get('icons');
+	icons[0].offset = (index/route.routes[0].overview_path.length) * 100 + '%';
+	console.log("offset = " +((index/route.routes[0].overview_path.length) * 100 + '%'));
+	line.set('icons', icons);
+}
+
 function updateGui(response, line)
 {
 	document.getElementById("ammountofcars").innerHTML = cars;
 	var carToAdd = document.createElement("p");
-	console.log(line);
+	//console.log(line);
 
 	var pathlenght = response.routes[0].overview_path.length;
 	var pathDurationSec = response.routes[0].legs[0].duration.value;
 	var pathStepDuration = pathDurationSec/pathlenght;
 
 
-	console.log(pathlenght);
-	console.log(pathDurationSec);
-	console.log(pathStepDuration);
-	console.log(response.routes[0].overview_path.forEach(function (value, index) { console.log(value.lat());
-                                                                                    console.log(value.lng());}));
+	//console.log(pathlenght);
+	//console.log(pathDurationSec);
+	//console.log(pathStepDuration);
+	//console.log(response.routes[0].overview_path.forEach(function (value, index) { console.log(value.lat());
+                                                                                    //console.log(value.lng());}));
 	carToAdd.innerHTML = "A car is driving from " + response.request.origin.query + " to " + response.request.destination.query;
 	document.getElementById("directions").appendChild(carToAdd);
 	//document.getElementById("simulation_waypointslist").appendChild(carToAdd);
